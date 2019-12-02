877ae519c42aff3b2d63feb5c36bb5b19a71706e
vera <pavlova.vera18@gmail.com>
2019-12-02 16:33:15 +0200

    Init comit

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (evant) => {
+	evant.preventDefault();
+	const location = searchElement.value
+
+	fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>{
+		response.json().then((data)=> {
+				if (data.error) {
+					messageOne.textContent = data.error;
+					messageTwo.textContent = '';	
+				} else {
+					messageOne.textContent = data.location;
+					messageTwo.textContent = data.forecast;					
+				}
+		})
+	})
+})
